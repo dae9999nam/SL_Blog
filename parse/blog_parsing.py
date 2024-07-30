9f93f334f414f1117d40f7c2fdc1743d097bf303
Seunghyeon Baek <u3582165@connect.hku.hk>
2024-07-30 22:58:04 +0900

    Blog parsing code Update

--- parse/blog_parsing.py
+++ parse/blog_parsing.py
@@ -2,7 +2,15 @@ import csv
 from bs4 import BeautifulSoup
 import requests
 
-
+# code needed to be fixed
 nset = [n for n in csv.reader(f)]
 
 #Print dataset
+print(nset)
+
+#Extract URLs from the dataset
+head = nset[0]
+data = nset[1:]
+
+for name, url in data:
+    print(url)
